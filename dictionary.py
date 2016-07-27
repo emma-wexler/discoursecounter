68a3897a11787a03e8dac6a426544dc6a3147935
Trevor D'Arcey <trevdarc@users.noreply.github.com>
2016-07-27 13:04:29 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,11 @@
 __author__ = 'emmawexler'
 
 # a list of discourse markers to search
-# must begin and end with a space
+# must begin and end with a space. This should be improved in the future, possibly by adding a set of
+# characters that are explicitly okay to surround the DM with (e.g., commas, spaces, quotes, asterisks)
 
-markers = [' um ' , ' like ', ' u:h ', ' u:m ', ' uh ', ' you know ', ' well ', ' so ', ' I mean ', ' oh ', ' as I say ',
-           ' great ', ' ok ', ' okay ', ' mind you ', ' cool ', ' and ', ' but ']
+markers = [' um ' , ' like ', ' u:h ', ' u:m ', ' uh ', ' you know ', ' well ', ' so ',
+           ' I mean ', ' oh ', ' as I say ', ' great ', ' ok ', ' okay ', ' mind you ',
+           ' cool ', ' and ', ' but ']
 
-
-
-
+# Other potential markers: then, because, although
